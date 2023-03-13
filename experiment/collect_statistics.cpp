c992be1ecb8d510b1f8f8eb5e9dc1ee617c4d74b
zhonghan wang <zhonghanwang@WZH-Macbook-Pro.local>
2023-03-13 11:59:39 +0800

    add label result

--- experiment/collect_statistics.cpp
+++ experiment/collect_statistics.cpp
@@ -2,15 +2,30 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 const string instance_path = "../ls_instance_server.txt";
 
 const string output_path = "z3_nra_newest.csv";
+const string output_label_path = "z3_nra_newest_label.csv";
 const string input_folder = "z3_nra_newest/"; 
 
 vector<string> instance_vec;
+unordered_map<string, string> m_ins_label;
+
+class label {
+public:
+    int num, num_arith, num_bool, num_solved, num_sat, num_unsat, num_unsolved;
+
+    label(): num(0), num_arith(0), num_bool(0), num_solved(0), num_sat(0), num_unsat(0), num_unsolved(0)
+    {
+
+    }
+};
+
+unordered_map<string, label *> m_labels;
 
 string get_instance_name(string str) {
     int idx1 = -1, idx2 = -1;
@@ -26,11 +41,40 @@ string get_instance_name(string str) {
     return str.substr(idx1 + 1, idx2 - idx1 - 1);
 }
 
+string get_label_name(string str) {
+    // /....../
+    int idx1 = -1, idx2 = -1;
+    // find QF_NRA2/..../
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] == '2') {
+            idx1 = i + 1;
+            break;
+        }
+    }
+    for(int i = idx1 + 1; i < str.length(); i++) {
+        if(str[i] == '/') {
+            idx2 = i;
+            break;
+        }
+    }
+    return str.substr(idx1 + 1, idx2 - idx1 - 1);
+}
+
+int self_stoi(string str) {
+    if(str.length() == 0 || !(str[0] >= '0' && str[0] <= '9')) {
+        return 0;
+    }
+    return stoi(str);
+}
+
 void read_instances() {
     ifstream inFile(instance_path);
-    string line;
+    string line; 
     while(getline(inFile, line)) {
-        instance_vec.push_back(get_instance_name(line));
+        auto ins_name = get_instance_name(line);
+        auto label_name = get_label_name(line);
+        instance_vec.push_back(ins_name);
+        m_ins_label[ins_name] = label_name;
     }
 }
 
@@ -48,12 +92,17 @@ string get_number(string str) {
     return idx2 != -1 ? str.substr(idx1, idx2 - idx1) : str.substr(idx1, str.length() - idx1);
 }
 
-void write_statistics() {
+void write_ins_statistics() {
     unsigned sat = 0, unsat = 0, unknown = 0, total = 0;
     ofstream outFile(output_path);
     outFile << "name, result, bool var, arith var, conflict, decision, stage, learned added\n";
     for(auto curr_instance: instance_vec) {
+        auto curr_label = m_ins_label[curr_instance];
+        if(m_labels.count(curr_label) == 0) {
+            m_labels[curr_label] = new label();
+        }
         total++;
+        m_labels[curr_label]->num++;
         ifstream inFile(input_folder + curr_instance + ".txt");
         string line;
         unsigned idx = 0;
@@ -84,15 +133,23 @@ void write_statistics() {
         }
         if(curr_result == "sat") {
             sat++;
+            m_labels[curr_label]->num_sat++;
+            m_labels[curr_label]->num_solved++;
         }
         else if(curr_result == "unsat") {
             unsat++;
+            m_labels[curr_label]->num_unsat++;
+            m_labels[curr_label]->num_solved++;
         }
         else {
             unknown++;
+            m_labels[curr_label]->num_unsolved++;
         }
         outFile << curr_instance << ", " << curr_result << ", " << curr_bool << ", " << curr_arith
         << ", " << curr_conf << ", " << curr_dec << ", " << curr_stg << ", " << curr_added << endl;
+
+        m_labels[curr_label]->num_bool += self_stoi(curr_bool);
+        m_labels[curr_label]->num_arith += self_stoi(curr_arith);
     }
     outFile.close();
     cout << "total: " << total << endl;
@@ -102,8 +159,19 @@ void write_statistics() {
     cout << "unknown: " << unknown << endl;
 }
 
+void write_label_statistics() {
+    ofstream outFile(output_label_path);
+    outFile << "name, total, bool var, arith var, sat, unsat, solved, unsolved\n";
+    for(auto ele: m_labels) {
+        outFile << ele.first << ", " << ele.second->num << ", " << ele.second->num_bool << ", " << ele.second->num_arith << ", "
+        << ele.second->num_sat << ", " << ele.second->num_unsat << ", " << ele.second->num_solved << ", " << ele.second->num_unsolved << endl;
+    }
+    outFile.close();
+}
+
 int main() {
     read_instances();
-    write_statistics();
+    write_ins_statistics();
+    write_label_statistics();
     return 0;
 }
